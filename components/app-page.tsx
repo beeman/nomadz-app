7acd44b7ea06f69a118abda0da5c9cb1d3ec347b
IlyaFriend <eliasbell2233@gmail.com>
2025-08-03 17:40:49 +0300

    chore: set app-page's bg color to black

--- components/app-page.tsx
+++ components/app-page.tsx
@@ -6,7 +6,7 @@ import { useAppThemeSpacing } from '@/components/use-app-theme-spacing'
 export function AppPage({ children, ...props }: PropsWithChildren<AppViewProps>) {
   const spacing = useAppThemeSpacing()
   return (
-    <AppView style={{ flex: 1, gap: spacing.md, padding: spacing.md }} {...props}>
+    <AppView style={{ flex: 1, gap: spacing.md, padding: spacing.md, backgroundColor: '#000000' }} {...props}>
       {children}
     </AppView>
   )
