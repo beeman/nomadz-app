4a4e12b1bbc6fc953074755e03d27b14035e5b16
IlyaFriend <eliasbell2233@gmail.com>
2025-08-03 18:59:28 +0300

    chore: remove settings tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -25,13 +25,6 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="settings"
-        options={{
-          title: 'Settings',
-          tabBarIcon: ({ color }) => <UiIconSymbol size={28} name="gearshape.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
         name="profile"
         options={{
           title: 'Profile',
